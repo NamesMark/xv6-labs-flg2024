a07574201a7c6fd168123abd98b44b3c39d67e6c
Mark Firman <mark.firman@braiins.cz>
2024-11-18 14:55:22 +0100

    kernel: syscall.c: Add trace

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -166,6 +166,10 @@ syscall(void)
   num = p->trapframe->a7;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     p->trapframe->a0 = syscalls[num]();
+    if (p->trace_mask & (1 << num)) {
+      printf("%d: syscall %s -> %d\n",
+            p->pid, syscall_name[num], p->trapframe->a0);
+    }
   } else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);
